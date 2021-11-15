ee2b753658a03f4c2c88bdc5c4249d7ddc24d2a9
islam <hammoudi.islam@live.fr>
2021-11-15 14:20:59 +0100

    unit tests

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -6,7 +6,7 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 
 class ExampleTest  extends TestCase
-{   
+{   //tests
     use WithoutMiddleware;
 
     public function testUnderAge()
